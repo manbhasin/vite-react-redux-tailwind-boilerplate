30bf9d101bb7ed6b8de1775bac1ad8c43b4774eb
Madhav Bhasin <madhav.bhasin@devslane.com>
2022-01-06 10:02:05 +0530

    [ADD] basic react and tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,27 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+    return (
+        <div className={'flex flex-col h-full'}>
+            <div className={'flex flex-col items-center justify-center h-4/5'}>
+                <span className={'text-4xl'}>Basic Setup</span>
+                <span className={'text-4xl'}>Vite + React + Tailwind + Redux</span>
+            </div>
+            <div className={'flex flex-col items-center justify-center'}>
+                <span className={'text-4xl'}>
+                    Made with <span className={'text-red-500'}> &#10084;</span> by{' '}
+                    <a
+                        className={'text-blue-400 underline'}
+                        href={'https://dev.page/madhav'}
+                        target={'_blank'}
+                        rel={'noopener noreferrer'}
+                    >
+                        Madhav
+                    </a>
+                </span>
+            </div>
+        </div>
+    );
+};
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
-}
-
-export default App
+export default App;
